55893af92d7e012f722a22417cca9e608a5d9c51
Deanout <dddehart@oakland.edu>
2017-12-29 05:04:42 +0000

    Working on server

---
+++ config/puma/development.rb
@@ -0,0 +1,18 @@
+def home_dir
+    '/home/ec2-user/dowhatdeansaysortheservergetsit'
+end
+
+def path(p)
+    File.join(home_dir, p)
+end
+
+directory home_dir
+environment 'develpment'
+
+pidfile path('tmp/pids/puma.pid')
+state_path path('tmp/pids/puma.state')
+stdout_redirect path('log/puma.log'), path('log/error.puma.log'), true
+threads 0, 1
+bind "unix:///home/ec2-user/dowhatdeansaysortheservergetsit/tmp/pids/puma.sock"
+activate_control_app
+
